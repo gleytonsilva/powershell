f299ef83f83c696b97f40763f6531f218766df42
gleytonsilva <30930643+gleytonsilva@users.noreply.github.com>
2023-05-18 15:33:00 -0300

    Add files via upload

---
+++ exemplo03_arrays.ps1
@@ -0,0 +1,16 @@
+﻿# script que usa Array
+# Comando para limpar a tela
+Clear-Host
+
+# Preenchendo um Array com servidores do googleDNS
+$google_dns = @("8.8.8.8", "8.8.4.4")
+$total_dns = $google_dns.Count
+Write-Host Pigando todos os $total_dns DNS do Google
+Test-Connection $google_dns -Count 1
+sleep 4
+Write-Host Fim!
+
+
+
+
+#fim
